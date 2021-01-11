ce595504d228f999edcc972738522780fbb86ba2
Brian H. Ward <glyphrider@gmail.com>
2021-01-11 15:12:18 -0500

    moved from distribution to os_family, allowing RedHat/Centos code to work for Amazon Linux

---
+++ libvirt/modules/amazon_linux/main.tf
@@ -0,0 +1,4 @@
+module "minecraft_host" {
+  source = "../minecraft_host"
+  base_volume_name = "amzn2-kvm-2.0.20201111.0-x86_64.xfs.gpt.qcow2"
+}
--- libvirt/modules/minecraft_host/main.tf
+++ libvirt/modules/minecraft_host/main.tf
@@ -8,7 +8,7 @@ module "host" {
   instance_name = "minecraft"
   memory = 16384
   vcpu = 8
-  disk = 8*1024*1024*1024
+  disk = 25*1024*1024*1024
   cloudinit_id = module.cloudinit.id
   base_volume_name = var.base_volume_name
 }
